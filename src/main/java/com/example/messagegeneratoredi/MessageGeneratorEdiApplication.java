b24c09e488821900e4d92824b2ccf30b679251b0
malikdurmus <98103955+malikdurmus@users.noreply.github.com>
2024-02-13 12:42:28 +0100

    initial commit1

---
+++ src/main/java/com/example/messagegeneratoredi/MessageGeneratorEdiApplication.java
@@ -0,0 +1,13 @@
+package com.example.messagegeneratoredi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageGeneratorEdiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageGeneratorEdiApplication.class, args);
+    }
+
+}
