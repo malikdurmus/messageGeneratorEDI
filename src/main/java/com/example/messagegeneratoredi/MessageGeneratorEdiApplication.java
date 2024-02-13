2f21041abf35d7f53946c39740afea84b63299cf
malikdurmus <98103955+malikdurmus@users.noreply.github.com>
2024-02-13 12:46:23 +0100

    second

--- src/main/java/com/example/messagegeneratoredi/MessageGeneratorEdiApplication.java
+++ src/main/java/com/example/messagegeneratoredi/MessageGeneratorEdiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MessageGeneratorEdiApplication {
 
     public static void main(String[] args) {
+        System.out.println(12);
         SpringApplication.run(MessageGeneratorEdiApplication.class, args);
     }
 
